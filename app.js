cb426ba04bafacecaeb3a8d22ba8a2225d81c577
Manikanta Cheruku <cherukumanikanta77@gmail.com>
2026-04-05 23:45:44 +0530

    visisto updated

--- app.js
+++ app.js
@@ -12,19 +12,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-
-  window.__VISISTO__ = {
-    site: "sk_e343532b594e6f9c23b6f4723a80e692bde1b6906fef6562",
-    api: "https://api.visisto.com"
-  };
-
-<script
-  src="https://cdn.visisto.com/loader.js"
-  data-site="sk_e343532b594e6f9c23b6f4723a80e692bde1b6906fef6562"
-  data-api="https://api.visisto.com"
-  defer
-></script>
-
-
-
